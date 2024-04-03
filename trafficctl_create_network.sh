7d2782aa5ef206fffb0af8944dcd5529f5962995
Xiaosheng Wu <xwu@confluent.io>
2024-04-03 11:29:22 -0700

    add create network script

--- trafficctl_create_network.sh
+++ trafficctl_create_network.sh
@@ -2,7 +2,7 @@
 CLOUD="AWS"   ## e.g "AWS"
 REGION="us-gov-east-1"  ## e.g "us-west-2"
 NETWORK_NAME="public-network-confluent-04-02" ## e.g public-network-confluent-<date>
-REALM="087359091383" ## cc-us-gov-infra-data-plane-1 /  e.g. GCP:cc-prod-1/AWS:050879227952
+REALM="account-id"
 
 network=$(trafficctl --ccloud infra-us-gov networking network create \
   --cloud "${CLOUD}" \
