5a50fa892203de1f60be761c2cae1aac607926a4
Xiaosheng Wu <xwu@confluent.io>
2024-04-21 23:34:27 -0700

    add config and find message

--- BCPDR/calculate_offset_by_timestamp.py
+++ BCPDR/calculate_offset_by_timestamp.py
@@ -1,14 +1,19 @@
 from kafka import TopicPartition, KafkaConsumer
 import time
+import sys
+import json
 
-def get_offset_for_timestamp(bootstrap_servers, topic, timestamp_ms):
-    consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers, 
-                             security_protocol='SASL_SSL',
-                             sasl_mechanism='PLAIN',
-                             sasl_plain_username='apikey',
-                             sasl_plain_password='apisecret',
+def build_consumer(): 
+    return KafkaConsumer(bootstrap_servers=config['bootstrap_servers'], 
+                             security_protocol=config['security_protocol'],
+                             sasl_mechanism=config['sasl_mechanism'],
+                             sasl_plain_username=config['sasl_plain_username'],
+                             sasl_plain_password=config['sasl_plain_password'],
                              api_version=(2,0,2), 
                              consumer_timeout_ms=1000)
+
+def get_offset_for_timestamp(topic, timestamp_ms):
+    consumer = build_consumer()
     offsets = {}
     try:
         topics = consumer.topics()
@@ -16,32 +21,56 @@ def get_offset_for_timestamp(bootstrap_servers, topic, timestamp_ms):
     except Exception as e:
         print("Error:", e)
     partitions = consumer.partitions_for_topic(topic)
-    print(partitions)
-    for partition in range(6):
+    print(f"Reading offsets from the following partitions {partitions}")
+    for partition in partitions:
         tp = TopicPartition(topic, partition)
-        print(tp)
         offsets[partition] = consumer.offsets_for_times({tp: timestamp_ms})
 
     consumer.close()
-    # admin_client.close()
     return offsets
 
+def find_target_message(topic, m_partition, m_offest):
+    consumer = build_consumer()
+    topic_partition = TopicPartition(topic, m_partition)
+    consumer.assign([topic_partition])
+    consumer.seek(topic_partition, m_offest)
+
+    # Read one message from the specified offset
+    consumer.poll(timeout_ms=1000)
+    for message in consumer:
+        print(f"Message from topic {topic} from partition {m_partition} at offset {m_offest}: {message}")
+        break
+    consumer.close()
+
 if __name__ == "__main__":
-    # Kafka bootstrap servers
-    bootstrap_servers = '<bootstrapserver>'
+    with open('config.json') as f:
+        config = json.load(f)
+    print("Configuration loaded:")
+    print(config)
 
-    # Kafka topic
-    topic = 'mirror_sample_data'
-
-    # Timestamp (in milliseconds since epoch)
-    timestamp_ms = 1713542362764  # current time
+    topic = config['topic']
+    timestamp_ms = config['target_timestamp']
 
     # Calculate offset based on timestamp
-    offsets = get_offset_for_timestamp(bootstrap_servers, topic, timestamp_ms)
+    offsets = get_offset_for_timestamp(topic, timestamp_ms)
     
+    last_message_timestamp = sys.maxsize
+    last_massage_partition = -1
+    last_message_offset = -1
     print("Offsets for timestamp {}: ".format(timestamp_ms))
     for partition_id, offset_and_timestamp in offsets.items():
         print(partition_id, offset_and_timestamp)
         offset_info = offset_and_timestamp[TopicPartition(topic, partition_id)]
         offset = offset_info.offset if offset_info else "N/A"
+        if (offset_info.timestamp <= last_message_timestamp):
+            last_massage_partition = partition_id
+            last_message_offset = offset
+            last_message_timestamp = offset_info.timestamp
         print(f"Partition {partition_id}: Offset {offset}")
+    if (last_massage_partition != -1 and last_message_offset != -1):
+        print("Last message at partition {} offset {} at timestamp {}".format(last_massage_partition, last_message_offset, last_message_timestamp))
+    else:
+        print("Error partition or offset equal to -1")
+
+    # poll the target message and find replay window left boundary
+    find_target_message(topic, last_massage_partition, last_message_offset)
